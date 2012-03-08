ee338a3f2a369bd92f233ede124db36719c08b17
Martin Provencher <mprovencher86@gmail.com>
2012-03-08 13:14:08 -0500

    Rails creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiceGame::Application.config.secret_token = '0594efbe11fb760f95e50ad806371bcfef584e5df9bac1463e871161726d103692f04e23ee4b35981873f84d37132be5b392c8780502d781f36d10aa76db6835'
